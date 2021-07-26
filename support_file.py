50c87ba14922713167bcae12547fbd4a8794d99e
laurent <laurent.mombaerts@gmail.com>
2021-07-26 17:19:21 +0200

    update new version

--- support_file.py
+++ support_file.py
@@ -253,6 +253,12 @@ def calc_wap2(df):
 def calc_wap3(df):
     return (df['bid_price2'] * df['bid_size2'] + df['ask_price2'] * df['ask_size2']) / (df['bid_size2'] + df['ask_size2'])
 
+def calc_wap4(df):
+    return (df['bid_price1'] * df['bid_size1'] + df['ask_price1'] * df['ask_size1']) / (df['bid_size1'] + df['ask_size1'])
+
+def mid_price(df):
+    return ((df['bid_price1'] + df['ask_price1']) / 2)
+
 def calc_rv_from_wap_numba(values, index):
     log_return = np.diff(np.log(values))
     realized_vol = np.sqrt(np.sum(np.square(log_return[1:])))
@@ -333,10 +339,23 @@ def financial_metrics(df):
     
     wap_imbalance = np.mean(df['wap'] - df['wap2'])
     price_spread = np.mean((df['ask_price1'] - df['bid_price1']) / ((df['ask_price1'] + df['bid_price1'])/2))
-    bid_spread = np.mean(df['bid_price1'] - df['bid_price2'])
-    ask_spread = np.mean(df['ask_price1'] - df['ask_price2'])
+    bid_spread = np.mean(df['bid_price1'] - df['bid_price2'])  
+    ask_spread = np.mean(df['ask_price1'] - df['ask_price2']) # Abs to take
     total_volume = np.mean((df['ask_size1'] + df['ask_size2']) + (df['bid_size1'] + df['bid_size2']))
     volume_imbalance = np.mean(abs((df['ask_size1'] + df['ask_size2']) - (df['bid_size1'] + df['bid_size2'])))
+    
+    return [wap_imbalance,price_spread,bid_spread,ask_spread,total_volume,volume_imbalance]
+
+def financial_metrics_2(df):
+    
+    wap_imbalance = np.mean(df['wap'] - df['wap2'])
+    price_spread = np.mean((df['ask_price1'] - df['bid_price1']) / ((df['ask_price1'] + df['bid_price1'])/2))
+    bid_spread = np.mean(df['bid_price1'] - df['bid_price2'])  
+    ask_spread = np.mean(df['ask_price1'] - df['ask_price2']) # Abs to take
+    total_volume = np.mean((df['ask_size1'] + df['ask_size2']) + (df['bid_size1'] + df['bid_size2']))
+    volume_imbalance = np.mean(abs((df['ask_size1'] + df['ask_size2']) - (df['bid_size1'] + df['bid_size2'])))
+    
+    # New features here
     
     return [wap_imbalance,price_spread,bid_spread,ask_spread,total_volume,volume_imbalance]
 
@@ -501,6 +520,198 @@ def computeFeatures_wEntropy(machine, dataset, all_stocks_ids, datapath):
             df_ent = pd.concat([times_pd,temp,temp2,temp3],axis=1)
             
         list_ent.append(df_ent)
+
+        # Compute other metrics
+        df_others = book_stock.groupby(['time_id']).apply(other_metrics).to_frame().reset_index().fillna(0)
+        df_others = df_others.rename(columns={0:'embedding'})
+        df_others[['linearFit1_1','linearFit1_2','linearFit1_3','wap_std1_1','wap_std1_2','wap_std1_3']] = pd.DataFrame(df_others.embedding.tolist(), index=df_others.index)
+        df_others['time_id'] = [f'{stock_id}-{time_id}' for time_id in df_others['time_id']] 
+        df_others = df_others.rename(columns={'time_id':'row_id'}).drop(['embedding'],axis=1)
+        list_others.append(df_others)
+
+        isEmpty = book_stock.query(f'seconds_in_bucket >= 300').empty
+        if isEmpty == False:
+            df_others2 = book_stock.query(f'seconds_in_bucket >= 300').groupby(['time_id']).apply(other_metrics).to_frame().reset_index().fillna(0)
+            df_others2 = df_others2.rename(columns={0:'embedding'})
+            df_others2[['linearFit2_1','linearFit2_2','linearFit2_3','wap_std2_1','wap_std2_2','wap_std2_3']] = pd.DataFrame(df_others2.embedding.tolist(), index=df_others2.index)
+            df_others2['time_id'] = [f'{stock_id}-{time_id}' for time_id in df_others2['time_id']] 
+            df_others2 = df_others2.rename(columns={'time_id':'row_id'}).drop(['embedding'],axis=1)
+        else:
+            times_pd = pd.DataFrame(all_time_ids_byStock,columns=['time_id'])
+            times_pd['time_id'] = [f'{stock_id}-{time_id}' for time_id in times_pd['time_id']]
+            times_pd = times_pd.rename(columns={'time_id':'row_id'})
+            temp = pd.DataFrame([0],columns=['linearFit2_1']) 
+            temp2 = pd.DataFrame([0],columns=['linearFit2_2'])
+            temp3 = pd.DataFrame([0],columns=['linearFit2_3'])
+            temp4 = pd.DataFrame([0],columns=['wap_std2_1'])
+            temp5 = pd.DataFrame([0],columns=['wap_std2_2'])
+            temp6 = pd.DataFrame([0],columns=['wap_std2_3'])
+            df_others2 = pd.concat([times_pd,temp,temp2,temp3,temp4,temp5,temp6],axis=1)
+            
+        list_others2.append(df_others2)
+
+        isEmpty = book_stock.query(f'seconds_in_bucket >= 480').empty 
+        if isEmpty == False:
+            df_others3 = book_stock.query(f'seconds_in_bucket >= 480').groupby(['time_id']).apply(other_metrics).to_frame().reset_index().fillna(0)
+            df_others3 = df_others3.rename(columns={0:'embedding'})
+            df_others3[['linearFit3_1','linearFit3_2','linearFit3_3','wap_std3_1','wap_std3_2','wap_std3_3']] = pd.DataFrame(df_others3.embedding.tolist(), index=df_others3.index)
+            df_others3['time_id'] = [f'{stock_id}-{time_id}' for time_id in df_others3['time_id']] 
+            df_others3 = df_others3.rename(columns={'time_id':'row_id'}).drop(['embedding'],axis=1)
+        else:
+            times_pd = pd.DataFrame(all_time_ids_byStock,columns=['time_id'])
+            times_pd['time_id'] = [f'{stock_id}-{time_id}' for time_id in times_pd['time_id']]
+            times_pd = times_pd.rename(columns={'time_id':'row_id'})
+            temp = pd.DataFrame([0],columns=['linearFit3_1']) 
+            temp2 = pd.DataFrame([0],columns=['linearFit3_2'])
+            temp3 = pd.DataFrame([0],columns=['linearFit3_3'])
+            temp4 = pd.DataFrame([0],columns=['wap_std3_1'])
+            temp5 = pd.DataFrame([0],columns=['wap_std3_2'])
+            temp6 = pd.DataFrame([0],columns=['wap_std3_3'])
+            df_others3 = pd.concat([times_pd,temp,temp2,temp3,temp4,temp5,temp6],axis=1)
+            
+        list_others3.append(df_others3)
+
+        print('Computing one stock took', time.time() - start, 'seconds for stock ', stock_id)
+
+    # Create features dataframe
+    df_submission = pd.concat(list_rv)
+    df_submission2 = pd.concat(list_rv2)
+    df_submission3 = pd.concat(list_rv3)
+    df_ent_concat = pd.concat(list_ent)
+    df_fin_concat = pd.concat(list_fin)
+    df_fin2_concat = pd.concat(list_fin2)
+    df_others = pd.concat(list_others)
+    df_others2 = pd.concat(list_others2)
+    df_others3 = pd.concat(list_others3)
+
+    df_book_features = df_submission.merge(df_submission2, on = ['row_id'], how='left').fillna(0)
+    df_book_features = df_book_features.merge(df_submission3, on = ['row_id'], how='left').fillna(0)
+    df_book_features = df_book_features.merge(df_ent_concat, on = ['row_id'], how='left').fillna(0)
+    df_book_features = df_book_features.merge(df_fin_concat, on = ['row_id'], how='left').fillna(0)
+    df_book_features = df_book_features.merge(df_fin2_concat, on = ['row_id'], how='left').fillna(0)
+    df_book_features = df_book_features.merge(df_others, on = ['row_id'], how='left').fillna(0)
+    df_book_features = df_book_features.merge(df_others2, on = ['row_id'], how='left').fillna(0)
+    df_book_features = df_book_features.merge(df_others3, on = ['row_id'], how='left').fillna(0)
+    
+    # Add encoded stock
+    encoder = np.eye(len(all_stocks_ids))
+    encoded = list()
+
+    for i in range(df_book_features.shape[0]):
+        stock_id = int(df_book_features['row_id'][i].split('-')[0])
+        encoded_stock = encoder[np.where(all_stocks_ids == int(stock_id))[0],:]
+        encoded.append(encoded_stock)
+
+    encoded_pd = pd.DataFrame(np.array(encoded).reshape(df_book_features.shape[0],np.array(all_stocks_ids).shape[0]))
+    df_book_features_encoded = pd.concat([df_book_features, encoded_pd],axis=1)
+    
+    return df_book_features_encoded
+
+def computeFeatures_july(machine, dataset, all_stocks_ids, datapath):
+    
+    list_rv, list_rv2, list_rv3 = [], [], []
+    list_ent, list_fin, list_fin2 = [], [], []
+    list_others, list_others2, list_others3 = [], [], []
+
+    for stock_id in range(127):
+        
+        start = time.time()
+        
+        if machine == 'local':
+            try:
+                book_stock = load_book_data_by_id(stock_id,datapath,dataset)
+            except:
+                continue
+        elif machine == 'kaggle':
+            try:
+                book_stock = load_book_data_by_id_kaggle(stock_id,dataset)
+            except:
+                continue
+        
+        # Useful
+        all_time_ids_byStock = book_stock['time_id'].unique() 
+
+        # Calculate wap for the book
+        book_stock['wap'] = calc_wap(book_stock)
+        book_stock['wap2'] = calc_wap2(book_stock)
+        book_stock['wap3'] = calc_wap3(book_stock)
+
+        # Calculate realized volatility
+        df_sub = book_stock.groupby('time_id')['wap'].agg(calc_rv_from_wap_numba, engine='numba').to_frame().reset_index()
+        df_sub2 = book_stock.groupby('time_id')['wap2'].agg(calc_rv_from_wap_numba, engine='numba').to_frame().reset_index()
+        df_sub3 = book_stock.groupby('time_id')['wap3'].agg(calc_rv_from_wap_numba, engine='numba').to_frame().reset_index()
+        df_sub['time_id'] = [f'{stock_id}-{time_id}' for time_id in df_sub['time_id']]
+        df_sub = pd.concat([df_sub,df_sub2['wap2'],df_sub3['wap3']],axis=1)
+        df_sub = df_sub.rename(columns={'time_id':'row_id','wap': 'rv', 'wap2': 'rv2', 'wap3': 'rv3'})
+        
+        # Calculate realized volatility last 5 min
+        isEmpty = book_stock.query(f'seconds_in_bucket >= 300').empty
+        if isEmpty == False:
+            df_sub_5 = book_stock.query(f'seconds_in_bucket >= 300').groupby(['time_id'])['wap'].agg(calc_rv_from_wap_numba, engine='numba').to_frame().reset_index()
+            df_sub2_5 = book_stock.query(f'seconds_in_bucket >= 300').groupby(['time_id'])['wap2'].agg(calc_rv_from_wap_numba, engine='numba').to_frame().reset_index()
+            df_sub3_5 = book_stock.query(f'seconds_in_bucket >= 300').groupby(['time_id'])['wap3'].agg(calc_rv_from_wap_numba, engine='numba').to_frame().reset_index()
+            df_sub_5['time_id'] = [f'{stock_id}-{time_id}' for time_id in df_sub_5['time_id']]
+            df_sub_5 = pd.concat([df_sub_5,df_sub2_5['wap2'],df_sub3_5['wap3']],axis=1)
+            df_sub_5 = df_sub_5.rename(columns={'time_id':'row_id','wap': 'rv_5', 'wap2': 'rv2_5', 'wap3': 'rv3_5'})
+        else: # 0 volatility
+            times_pd = pd.DataFrame(all_time_ids_byStock,columns=['time_id'])
+            times_pd['time_id'] = [f'{stock_id}-{time_id}' for time_id in times_pd['time_id']]
+            times_pd = times_pd.rename(columns={'time_id':'row_id'})
+            zero_rv = pd.DataFrame(np.zeros((1,times_pd.shape[0])),columns=['rv_5'])
+            zero_rv2 = pd.DataFrame(np.zeros((1,times_pd.shape[0])),columns=['rv2_5'])
+            zero_rv3 = pd.DataFrame(np.zeros((1,times_pd.shape[0])),columns=['rv3_5'])
+            df_sub_5 = pd.concat([times_pd,zero_rv,zero_rv2,zero_rv3],axis=1) 
+
+        # Calculate realized volatility last 2 min
+        isEmpty = book_stock.query(f'seconds_in_bucket >= 480').empty
+        if isEmpty == False:
+            df_sub_2 = book_stock.query(f'seconds_in_bucket >= 480').groupby(['time_id'])['wap'].agg(calc_rv_from_wap_numba, engine='numba').to_frame().reset_index()
+            df_sub2_2 = book_stock.query(f'seconds_in_bucket >= 480').groupby(['time_id'])['wap2'].agg(calc_rv_from_wap_numba, engine='numba').to_frame().reset_index()
+            df_sub3_2 = book_stock.query(f'seconds_in_bucket >= 480').groupby(['time_id'])['wap3'].agg(calc_rv_from_wap_numba, engine='numba').to_frame().reset_index()    
+            df_sub_2['time_id'] = [f'{stock_id}-{time_id}' for time_id in df_sub_2['time_id']] 
+            df_sub_2 = pd.concat([df_sub_2,df_sub2_2['wap2'],df_sub3_2['wap3']],axis=1)
+            df_sub_2 = df_sub_2.rename(columns={'time_id':'row_id','wap': 'rv_2', 'wap2': 'rv2_2', 'wap3': 'rv3_2'})
+        else: # 0 volatility
+            times_pd = pd.DataFrame(all_time_ids_byStock,columns=['time_id'])
+            times_pd['time_id'] = [f'{stock_id}-{time_id}' for time_id in times_pd['time_id']]
+            times_pd = times_pd.rename(columns={'time_id':'row_id'})
+            zero_rv = pd.DataFrame(np.zeros((1,times_pd.shape[0])),columns=['rv_2'])
+            zero_rv2 = pd.DataFrame(np.zeros((1,times_pd.shape[0])),columns=['rv2_2'])
+            zero_rv3 = pd.DataFrame(np.zeros((1,times_pd.shape[0])),columns=['rv3_2'])
+            df_sub_2 = pd.concat([times_pd,zero_rv,zero_rv2,zero_rv3],axis=1) 
+
+        list_rv.append(df_sub)
+        list_rv2.append(df_sub_5)
+        list_rv3.append(df_sub_2)
+
+        # Calculate other financial metrics from book 
+        df_sub_book_feats = book_stock.groupby(['time_id']).apply(financial_metrics_2).to_frame().reset_index()
+        df_sub_book_feats = df_sub_book_feats.rename(columns={0:'embedding'})
+        df_sub_book_feats[['wap_imbalance','price_spread','bid_spread','ask_spread','total_vol','vol_imbalance']] = pd.DataFrame(df_sub_book_feats.embedding.tolist(), index=df_sub_book_feats.index)
+        df_sub_book_feats['time_id'] = [f'{stock_id}-{time_id}' for time_id in df_sub_book_feats['time_id']] 
+        df_sub_book_feats = df_sub_book_feats.rename(columns={'time_id':'row_id'}).drop(['embedding'],axis=1)
+
+        isEmpty = book_stock.query(f'seconds_in_bucket >= 300').empty
+        if isEmpty == False:
+            df_sub_book_feats5 = book_stock.query(f'seconds_in_bucket >= 300').groupby(['time_id']).apply(financial_metrics_2).to_frame().reset_index()
+            df_sub_book_feats5 = df_sub_book_feats5.rename(columns={0:'embedding'})
+            df_sub_book_feats5[['wap_imbalance5','price_spread5','bid_spread5','ask_spread5','total_vol5','vol_imbalance5']] = pd.DataFrame(df_sub_book_feats5.embedding.tolist(), index=df_sub_book_feats5.index)
+            df_sub_book_feats5['time_id'] = [f'{stock_id}-{time_id}' for time_id in df_sub_book_feats5['time_id']] 
+            df_sub_book_feats5 = df_sub_book_feats5.rename(columns={'time_id':'row_id'}).drop(['embedding'],axis=1)
+        else:
+            times_pd = pd.DataFrame(all_time_ids_byStock,columns=['time_id'])
+            times_pd['time_id'] = [f'{stock_id}-{time_id}' for time_id in times_pd['time_id']]
+            times_pd = times_pd.rename(columns={'time_id':'row_id'})
+            temp = pd.DataFrame([0],columns=['wap_imbalance5']) 
+            temp2 = pd.DataFrame([0],columns=['price_spread5'])
+            temp3 = pd.DataFrame([0],columns=['bid_spread5'])
+            temp4 = pd.DataFrame([0],columns=['ask_spread5'])
+            temp5 = pd.DataFrame([0],columns=['total_vol5'])
+            temp6 = pd.DataFrame([0],columns=['vol_imbalance5'])
+            df_sub_book_feats5 = pd.concat([times_pd,temp,temp2,temp3,temp4,temp5,temp6],axis=1) 
+            
+        list_fin.append(df_sub_book_feats)
+        list_fin2.append(df_sub_book_feats5)
 
         # Compute other metrics
         df_others = book_stock.groupby(['time_id']).apply(other_metrics).to_frame().reset_index().fillna(0)
