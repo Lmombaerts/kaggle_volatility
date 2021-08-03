574639b4f73cd6df02ccb45ae4f93d2bd40bf2c7
Vladimir Levin <vladimir.levin@uni.lu>
2021-08-03 20:50:17 +0200

    update R file (faster data load)

--- features_analysis.R
+++ features_analysis.R
@@ -2,19 +2,32 @@ library(data.table)
 library(magrittr)
 library(Hmisc)
 
-# Read the .csv file
-features <- fread(paste0(getwd(), "/df_features_train.csv"))
 
-# set the name to the index column
-colnames(features)[1] <- "index"
+# File load ---------------------------------------------------------------
 
+
+# # Read the .csv file
+# features <- fread(paste0(getwd(), "/df_features_train.csv")) # 20.5 sec loading time
+# 
+# # set the name to the index column
+# colnames(features)[1] <- "index"
+# 
 # # output classes of features in the .csv file
 # fwrite(features[, lapply(.SD, class)], "~/Desktop/temp.csv")
+# 
+# saveRDS(features, file=paste0(getwd(), "/features.RDS"))
 
-# analyze the features
+# or read .RDS file
+features <- readRDS(paste0(getwd(), "/features.RDS")) # 5.6 sec loading time
+
+
+
+# feature analysis --------------------------------------------------------
+
 features[, describe(trade_roll_measure)]
 features[, histogram(trade_roll_measure)]
 features[trade_roll_measure < 0.002, histogram(trade_roll_measure)]
+
 
 
 
